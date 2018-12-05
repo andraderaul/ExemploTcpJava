91d4943562c605398059eef87127e26cc9ed7cac
Raul Andrade <theandraderaul@gmail.com>
2018-12-05 03:14:15 +0000

    adicionando funcionalidades sugeridas pelo professor

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -5,21 +5,59 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
+
 public class TCPClient{
     public static void main(String[] args){
         try {
             System.out.print("[ Conectando com o Servidor TCP    ..................  ");
+            /* 
+                Primeiro paramentro é o ip da maquina local
+                Segundo paramentro é a porta do servidor
+                e tem outro construtor alem desse utilizado
+                
+                Cria o socket e a conexao
+            */
             Socket sock = new Socket("127.0.0.1", 3300);
             System.out.println("[OK] ]");
             
             InputStream is = sock.getInputStream(); // Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); // Canal de saída de dados
-            String msg = "Olá, DCOMP!!!";
+            String msg = "Olá, Servidor!!!";
             byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
 
             System.out.print("[ Enviando mensagem    ..............................  ");
             os.write(buf);
             System.out.println("[OK] ]");
+            
+            // ------------- MINHA ALTERACAO -------------
+            buf = new byte[20];
+            is.read(buf);
+            msg = new String(buf);
+            System.out.println(" Mensagem recebida: "+ msg);
+            
+            // chat amador
+            int i = 0;
+            
+            while(i < 5 ) {
+                
+                // escrevendo e enviando a msg
+                System.out.print(" Messagem enviada: ");
+                Scanner se = new Scanner(System.in);
+                msg = se.nextLine();
+                buf = msg.getBytes();
+                os.write(buf);
+                
+                // recebendo a msg
+                buf = new byte[20];
+                is.read(buf);
+                msg = new String(buf);
+                System.out.println(" Mensagem recebida: "+ msg);
+                
+                i++;
+            }
+            
+            
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
@@ -5,18 +5,32 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
+
 public class TCPServer{
     public static void main(String[] args){
         
         try {
             System.out.print("[ Iniciando Servidor TCP    .........................  ");
-            ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1"));
+            /* 
+                o primeiro paramentro é a porta que o servidor ta escutando
+                o segundo paramentro é o tamanho da fila que o listen vai usar internamente
+                o terceiro paramentro é o IP da maquina local
+                
+                Serversocket faz o SOCKET, O BIND E O LISTEN
+            */
+            ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1")); 
             System.out.println("[OK] ]");
             
             System.out.print("[ Aquardando pedidos de conexão    ..................  ");
+            /* 
+                Aqui ele faz o pedido de conexao , utilizando o serversocket declarado logo acima
+                Socket é so do javanet e é chamado assim no TCP no UDP é DatagramaSocket
+            */
             Socket sock = ss.accept(); // Operação bloqueante (aguardando pedido de conexão)
             System.out.println("[OK] ]");
             
+            /* InputStream e OutputStream é o fluxo de bytes */
             InputStream is = sock.getInputStream(); //Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); //Canal de saída de dados
             byte[] buf = new byte[20]; // buffer de recepção
@@ -28,6 +42,34 @@ public class TCPServer{
             String msg = new String(buf); // Mapeando vetor de bytes recebido para String
             
             System.out.println("  Mensagem recebida: "+ msg);
+            
+            // ------------- MINHA ALTERACAO -------------
+            msg = "Ola, Client";
+            buf = msg.getBytes();
+            os.write(buf);
+            
+            
+            // chat amador
+            int i = 0;
+            
+            while (i < 5) {
+                
+                // recebendo a msg
+                buf = new byte[20];
+                is.read(buf);
+                msg = new String(buf);
+                System.out.println(" Mensagem recebida: "+ msg);
+                
+                // escrevendo e enviando a msg
+                System.out.print(" Messagem enviada: ");
+                Scanner se = new Scanner(System.in);
+                msg = se.nextLine();
+                buf = msg.getBytes();
+                os.write(buf);
+                
+                i++;
+            }
+            
         }catch(Exception e){System.out.println(e);}    
         System.out.println("[ FIM ]");
     }
